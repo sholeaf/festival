15e78b0e701acca226156e808c982f8e86106637
sholeaf <ohgoo4198@naver.com>
2024-11-08 09:11:46 +0900

    저장

--- front_festival/src/pages/Festival.jsx
+++ front_festival/src/pages/Festival.jsx
@@ -1,15 +1,23 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import Calendar from 'react-calendar';
 import TodayDate from "../hooks/TodayDate";
 
+// API 관련
+const API_URL = 'https://apis.data.go.kr/B551011/KorService1/searchFestival1?';
+const API_KEY = 'ADUQciriMbR143Lb7A8xLWVlcBZQXuCPTgGmksfopPBMwtmLQhkIrGlBror4PosCYnLLVqtrEnZz1T%2F4N9atVg%3D%3D';
+
+
 //축제 달력 컴포넌트
 const FestivalCalendar = ({ setParam, param }) => {
+
+    const [festivals, setFestivals] = useState([]);
     // 날짜 선택 시 처리 함수
     const handleDateChange = (date) => {
         // 선택한 날짜를 YYYYMMDD 형식으로 변환
-        const selectedDate = date.getFullYear().toString() + 
-                             (date.getMonth() + 1).toString().padStart(2, '0') + 
-                             date.getDate().toString().padStart(2, '0'); // YYYYMMDD 형식
+        const selectedDate = date.getFullYear().toString() +
+            (date.getMonth() + 1).toString().padStart(2, '0') +
+            date.getDate().toString().padStart(2, '0'); // YYYYMMDD 형식
 
         // setParam을 호출하여 eventStartDate를 업데이트
         setParam({
@@ -20,11 +28,53 @@ const FestivalCalendar = ({ setParam, param }) => {
 
     console.log('변경된 날짜 : ', param.eventStartDate);
 
+    useEffect(() => {
+        console.log('param.eventStart : ',param.eventStartDate);
+        axios.get(`${API_URL}serviceKey=${API_KEY}&numOfRows=${param.numOfRow}&pageNo=${param.pageNo}&MobileOS=${param.MobileOS}&MobileApp=${param.MobileApp}&_type=json&listYN=Y&arrange=A&eventStartDate=${param.eventStartDate}`)
+            .then((resp) => {
+                console.log("resp: " ,resp);
+                const festivalsData = resp.data.response.body.items.item|| [];
+                console.log("item: " ,festivalsData);
+
+                setFestivals(festivalsData);
+            })
+
+    }, [param]);
+
     return (
-        <>
+        <div>
             <Calendar onChange={handleDateChange} />
-            <div>축제 달력</div>
-        </>
+
+            {/* 축제 목록 */}
+            {festivals.length > 0 && (
+                <div>
+                    <h3>축제 목록</h3>
+                    <ul>
+                        {festivals.map((festival) => (
+                            <li key={festival.contentid}>
+                                <h4>{festival.title}</h4>
+                                <p>{festival.addr1}</p>
+                                {festival.firstimage != null && festival.firstimage !== "" ?
+                                    (<img
+                                    src={festival.firstimage}
+                                    alt={festival.title}
+                                    style={{ width: "100px", height: "100px" }}
+                                />) :
+                                (<img src="front_festival\src\assets\images\no-image.jpg"></img>)
+                                }
+                                
+                                <p>
+                                    {festival.eventstartdate} ~ {festival.eventenddate}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+
+            {/* 축제 목록이 없을 때 */}
+            {festivals.length === 0 && <p>해당 날짜에 대한 축제가 없습니다.</p>}
+        </div>
     );
 }
 
@@ -46,8 +96,6 @@ const FestivalSearch = () => {
 
 
 const Festival = () => {
-    const API_URL = 'http://apis.data.go.kr/B551011/KorService1/';
-    const API_KEY = 'ADUQciriMbR143Lb7A8xLWVlcBZQXuCPTgGmksfopPBMwtmLQhkIrGlBror4PosCYnLLVqtrEnZz1T%2F4N9atVg%3D%3D';
 
     const { noHyphen } = TodayDate();
 
@@ -61,8 +109,6 @@ const Festival = () => {
         pageNo: 1, // 페이지 num
         MobileOS: 'ETC',
         MobileApp: 'AppTest',
-        type: 'json',
-        listYN: 'Y', // 목록으로
         arrange: 'A', // 제목순
         eventStartDate: noHyphen, // YYYYMMDD 형식의 시작 날짜
         eventEndDate: '', // 기본 값은 없음 값이 있다면 YYYYMMDD 형식
