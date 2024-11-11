5ab5154054effb27c87fafd97bb05babd6358ca4
sholeaf <ohgoo4198@naver.com>
2024-11-11 16:44:25 +0900

    지도 추가

--- front_festival/src/pages/festival/Festival.jsx
+++ front_festival/src/pages/festival/Festival.jsx
@@ -3,20 +3,12 @@ import { useEffect, useRef, useState } from "react";
 import Header from "../../layout/Header";
 import TodayDate from "../../hooks/TodayDate";
 import FestivalCalendar from "./FestivalCalendar"
+import FestivalMap from "./FestivalMap";
 
 // API 관련
 const API_URL = 'https://apis.data.go.kr/B551011/KorService1/searchFestival1?MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&';
 const API_KEY = 'ADUQciriMbR143Lb7A8xLWVlcBZQXuCPTgGmksfopPBMwtmLQhkIrGlBror4PosCYnLLVqtrEnZz1T%2F4N9atVg%3D%3D';
 
-
-
-
-//축제 지도 컴포넌트
-const FestivalMap = () => {
-    return (
-        <div>축제 지도</div>
-    );
-}
 
 
 //축제 검색 컴포넌트
@@ -33,7 +25,7 @@ const Festival = () => {
     const todayRef = useRef(noHyphen);
 
     const [param, setParam] = useState({
-        numOfRow: 20, //페이지 결과 수
+        numOfRow: 10, //페이지 결과 수
         pageNo: 1, // 페이지 num
         eventStartDate: noHyphen, // YYYYMMDD 형식의 시작 날짜
         eventEndDate: '', // 기본 값은 없음 값이 있다면 YYYYMMDD 형식
@@ -50,6 +42,15 @@ const Festival = () => {
             }));
         }
     }, [activeTab, noHyphen]);
+    useEffect(() => {
+        if (activeTab !== 'map') {
+            setParam(prevParam => ({
+                ...prevParam,
+                eventStartDate: noHyphen, // 현재 날짜로 설정
+                areaCode: '',
+            }));
+        }
+    }, [activeTab, noHyphen]);
     return (
         <>
             <Header />
@@ -62,7 +63,7 @@ const Festival = () => {
 
                 <div className="content">
                     {activeTab === 'calendar' && <FestivalCalendar setParam={setParam} param={param} API_URL={API_URL} API_KEY={API_KEY} />}
-                    {activeTab === 'map' && <FestivalMap />}
+                    {activeTab === 'map' && <FestivalMap setParam={setParam} param={param} API_URL={API_URL} API_KEY={API_KEY} noHyphen={noHyphen}/>}
                     {activeTab === 'search' && <FestivalSearch />}
                 </div>
             </div>
