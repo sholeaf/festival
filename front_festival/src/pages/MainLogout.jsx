aedca528258097408de70fb2e1e6fe5a2de7c95f
sholeaf <ohgoo4198@naver.com>
2024-11-22 13:31:20 +0900

    스타일 메인 추가

--- front_festival/src/pages/MainLogout.jsx
+++ front_festival/src/pages/MainLogout.jsx
@@ -26,8 +26,20 @@ const MainLogout = () => {
                     <div className="main-title">모두의 축제</div>
                 )}
                 <div className="main-section">
-                    <div className="main-text">국내 모든 축제들을 찾아보세요!</div>
                     <img className="main-text-img" src="" />
+                    <div className="main-text1">국내 모든 축제들을 찾아보세요!</div>
+                    <div className="main-text2">다양한 축제들이 준비되어있어요!</div>
+                    <div className="go-page-btn">바로가기</div>
+                </div>
+                <div className="main-section">
+                    <img className="main-text-img" src="" />
+                    <div className="main-text1">축제를 다녀온 많은 사람들의 후기!</div>
+                    <div className="main-text2">축제 후기를 작성해서 사람들에게 알려주세요!</div>
+                    <div className="go-page-btn">바로가기</div>
+                </div>
+                <div className="main-section">
+                    <img className="main-text-img" src="" />
+                    <div className="main-text1">축제 내용 변경 등 중요사항을 공지해드립니다!</div>
                     <div className="go-page-btn">바로가기</div>
                 </div>
 
